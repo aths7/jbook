817c3892672a46cb3b33debfb332522bd3cfc795
Atharva Wankhede <atharva.wankhede@ganitinc.com>
2024-10-21 13:29:50 +0530

    taking input from UI

--- src/index.tsx
+++ src/index.tsx
@@ -1,53 +1,59 @@
-import * as esbuild from 'esbuild-wasm'
-import { createRoot } from "react-dom/client"
-import { useState, useEffect, useRef } from "react"
+import * as esbuild from "esbuild-wasm";
+import { createRoot } from "react-dom/client";
+import { useState, useEffect, useRef } from "react";
 
-import { unpkgPathPlugin } from './plugins/unpkg-path-plugin'
-
+import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 
 const App = () => {
-    const [input, setInput] = useState<string>('');
-    const [code, setCode] = useState();
-    const ref = useRef<any>();
+  const [input, setInput] = useState<string>("");
+  const [code, setCode] = useState();
+  const ref = useRef<any>();
 
-    const startService = async () => {
-        ref.current = await esbuild.startService({
-            worker: true,
-            wasmURL: '/esbuild.wasm'
-        })
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: "/esbuild.wasm",
+    });
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
     }
 
-    useEffect(() => {
-        startService();
-    }, [])
+    // const result = await ref.current.transform(input, { loader: 'jsx', target: 'es2015', })
+    const result = await ref.current.build({
+      entryPoints: ["index.js"],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin(input)],
+      define: {
+        "process.env.NODE_ENV": '"production"',
+        global: "window",
+      },
+    });
+    console.log(result);
+    setCode(result.outputFiles[0].text);
+  };
 
-    const onClick = async () => {
-        if (!ref.current) {
-            return
-        }
+  return (
+    <div>
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
+};
 
-        // const result = await ref.current.transform(input, { loader: 'jsx', target: 'es2015', })
-        const result = await ref.current.build({
-            entryPoints: ['index.js'],
-            bundle: true,
-            write: false,
-            plugins: [unpkgPathPlugin()],
-            define: {
-                'process.env.NODE_ENV': '"production"',
-                global: 'window'
-            }
-        })
-        console.log(result)
-        setCode(result.outputFiles[0].text)
-    }
-
-    return (<div>
-        <textarea value={input} onChange={(e) => setInput(e.target.value)}></textarea>
-        <div><button onClick={onClick}>Submit</button></div>
-        <pre>{code}</pre>
-    </div>)
-}
-
-const container = document.getElementById('root')
-const root = createRoot(container!)
-root.render(<App />)
+const container = document.getElementById("root");
+const root = createRoot(container!);
+root.render(<App />);
