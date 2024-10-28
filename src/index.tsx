a3f14d458e8bfd93f152ad5270b2bf6882ca6ede
Atharva Wankhede <atharva.wankhede@ganitinc.com>
2024-10-28 15:12:45 +0530

    adding states

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,20 @@ import { createRoot } from "react-dom/client";
 import "bulmaswatch/superhero/bulmaswatch.min.css";
 // import "bulmaswatch/cerulean/bulmaswatch.min.css";
 
+import { Provider } from "react-redux";
+import { store } from "./state";
+
 // import CodeCell from "./components/code-cell";
 import TextEditor from "./components/text-editor";
 
 const App = () => {
   return (
-    <div>
-      {/* <CodeCell /> */}
-      <TextEditor />
-    </div>
+    <Provider store={store}>
+      <div>
+        {/* <CodeCell /> */}
+        <TextEditor />
+      </div>
+    </Provider>
   );
 };
 
