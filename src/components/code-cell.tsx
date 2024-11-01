d66ef323b338b96b4f1555f85df9401ac52de95d
aths7 <atharvaw007@gmail.com>
2024-11-01 14:19:03 +0530

    adding extra details

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -33,7 +33,15 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
       <div style={{ height: "100%", display: "flex", flexDirection: "row" }}>
         <Resizable direction="horizontal">
           <CodeEditor
-            initialValue={cell.content}
+            initialValue={cell.content || `import React from 'react';
+import {createRoot } from 'react-dom/client'
+const App = () =>{
+  return <h1> Hello  !!</h1>
+}
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
+`}
             onChange={(value) => updateCell(cell.id, value)}
           />
         </Resizable>
